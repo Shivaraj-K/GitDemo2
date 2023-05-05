8eafee97c3249dbe7290d535645a4cfed5c24b76
Shivaraj-K <kshivu741@gmail.com>
2023-05-05 12:16:32 +0530

    commit to

---
+++ src/DEmo.java
@@ -0,0 +1,16 @@
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DEmo {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\SHIVARAJ GUTTEDAR\\Downloads\\chromedriver_win32 (1)\\chromedriverSP.exe");
+		WebDriver d=new ChromeDriver();
+		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		d.manage().window().maximize();
+		d.get("https://rahulshettyacademy.com/dropdownsPractise/");
+	}
+}
