08e91188d3b290131d64bfcbaa15f4f733cb7766
Shivaraj-K <kshivu741@gmail.com>
2023-05-05 15:20:03 +0530

    Indian

--- src/DEmo.java
+++ src/DEmo.java
@@ -7,10 +7,11 @@ public class DEmo {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\SHIVARAJ GUTTEDAR\\Downloads\\chromedriver_win32 (1)\\chromedriverSP.exe");
-		WebDriver d=new ChromeDriver();
-		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		d.manage().window().maximize();
-		d.get("https://rahulshettyacademy.com/dropdownsPractise/");
+//		System.setProperty("webdriver.chrome.driver","C:\\Users\\SHIVARAJ GUTTEDAR\\Downloads\\chromedriver_win32 (1)\\chromedriverSP.exe");
+//		WebDriver d=new ChromeDriver();
+//		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//		d.manage().window().maximize();
+//		d.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		System.out.println("Hello");
 	}
 }
